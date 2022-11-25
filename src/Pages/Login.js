dce4c1fad9a49d948201caf0246ac67cd846e192
Ranti <rantiprihatiningsih15@gmail.com>
2022-11-26 00:12:11 +0700

    fix form login cuy

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,26 +1,60 @@
 import "@fontsource/poppins";
-import React from "react";
+import React, { useState } from "react";
 import "./tes.css";
 
 
 const Login = () => {
-  {
-    return (
-      <div className="login" id="login">
-        <div className='container mx-auto  max-w-screen-3xl'>
-          <div class="flex">
-            <div className="flex-1 p-9">
-              <img src="fotoo.png" className="foto-login" />
-            </div>
-            <div class="flex-1 p-9 relative">
-              <div class="box-border h-[100%] w-5/6 p-16 bg-white absolute right-0 rounded-[20px] shadow-[-4px_4px_15px_rgba(0,0,0,0.09)]">
-                <h2 className="bg-inherit text-[25px] leading-[38px] font-bold text-[#075B61] text-center mb-4">REGISTER</h2>
+  const [nama, setNama] = useState('');
+  const [jenisKelamin, setJenisKelamin] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const resetValue = () => {
+    setNama('');
+    setJenisKelamin('');
+    setEmail('');
+    setEmail('');
+    setPassword('');
+    setConfirmPassword('');
+  }
+  const handleSave = (e) => {
+    e.preventDefault();
+    const objData = {
+      nama,
+      jenisKelamin,
+      email,
+      phone,
+      password,
+      confirmPassword,
+    }
+    console.log(JSON.stringify(objData));
+    resetValue();
+  }
+
+  return (
+    <div className="login" id="login">
+      <div className='container mx-auto  max-w-screen-3xl'>
+        <div class="flex">
+          <div className="flex-1 p-9">
+            <img src="fotoo.png" className="foto-login" />
+          </div>
+          <div class="flex-1 p-9 relative">
+            <div class="box-border h-[100%] w-5/6 p-16 bg-white absolute right-0 rounded-[20px] shadow-[-4px_4px_15px_rgba(0,0,0,0.09)]">
+              <h2 className="bg-inherit text-[25px] leading-[38px] font-bold text-[#075B61] text-center mb-4">REGISTER</h2>
+              <form onSubmit={handleSave} className='bg-inherit'>
                 <div className="bg-inherit gap-4 mb-8">
                   <div className="bg-inherit not-italic font-medium text-[#075B61] text-base">
                     <label htmlFor="nama" className="bg-inherit ">Nama Lengkap</label>
                   </div>
                   <div className="bg-inherit">
-                    <input type='text' className="bg-inherit rounded-lg border-solid border-[1px] border-[#075B61] opacity-[30%] w-full h-[49px] px-4 text-black" />
+                    <input
+                      name="nama"
+                      value={nama}
+                      onChange={(e) => setNama(e.target.value)}
+                      type='text'
+                      className="bg-inherit rounded-lg border-solid border-[1px] border-[#075B61] opacity-[30%] w-full h-[49px] px-4 text-black" />
                   </div>
                 </div>
                 <div className="bg-inherit gap-4 mb-8">
@@ -28,7 +62,13 @@ const Login = () => {
                     <label htmlFor="nama" className="bg-inherit ">Jenis Kelamin</label>
                   </div>
                   <div className="bg-inherit">
-                    <select type='select' className="bg-inherit rounded-lg border-solid border-[1px] border-[#075B61] opacity-[30%] w-full h-[49px] px-4 text-[#075B61] " >
+                    <select
+                      name="jenis_kelamin"
+                      value={jenisKelamin}
+                      onChange={(e) => setJenisKelamin(e.target.value)}
+                      type='select'
+                      className="bg-inherit rounded-lg border-solid border-[1px] border-[#075B61] opacity-[30%] w-full h-[49px] px-4 text-[#075B61] "
+                    >
                       <option value='perempuan'>Perempuan</option>
                       <option value='laki-laki'>Laki-Laki</option>
                     </select>
@@ -39,7 +79,12 @@ const Login = () => {
                     <label htmlFor="nama" className="bg-inherit ">Email</label>
                   </div>
                   <div className="bg-inherit">
-                    <input type='text' className="bg-inherit rounded-lg border-solid border-[1px] border-[#075B61] opacity-[30%] w-full h-[49px] px-4 text-[#075B61]" />
+                    <input
+                      name="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      type='text'
+                      className="bg-inherit rounded-lg border-solid border-[1px] border-[#075B61] opacity-[30%] w-full h-[49px] px-4 text-[#075B61]" />
                   </div>
                 </div>
                 <div className="bg-inherit gap-4 mb-8">
@@ -47,7 +92,12 @@ const Login = () => {
                     <label htmlFor="nama" className="bg-inherit ">Nomor Telepon</label>
                   </div>
                   <div className="bg-inherit">
-                    <input type='text' className="bg-inherit rounded-lg border-solid border-[1px] border-[#075B61] opacity-[30%] w-full h-[49px] px-4 text-[#075B61]" />
+                    <input
+                      name="number"
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                      type='number'
+                      className="bg-inherit rounded-lg border-solid border-[1px] border-[#075B61] opacity-[30%] w-full h-[49px] px-4 text-[#075B61]" />
                   </div>
                 </div>
                 <div className="bg-inherit gap-4 mb-8">
@@ -55,7 +105,12 @@ const Login = () => {
                     <label htmlFor="nama" className="bg-inherit ">Kata Sandi</label>
                   </div>
                   <div className="bg-inherit">
-                    <input type='text' className="bg-inherit rounded-lg border-solid border-[1px] border-[#075B61] opacity-[30%] w-full h-[49px] px-4 text-[#075B61]" />
+                    <input
+                      name="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      type='text'
+                      className="bg-inherit rounded-lg border-solid border-[1px] border-[#075B61] opacity-[30%] w-full h-[49px] px-4 text-[#075B61]" />
                   </div>
                 </div>
                 <div className="bg-inherit gap-4 mb-12">
@@ -63,20 +118,33 @@ const Login = () => {
                     <label htmlFor="nama" className="bg-inherit ">Konfirmasi Kata Sandi</label>
                   </div>
                   <div className="bg-inherit">
-                    <input type='text' className="bg-inherit rounded-lg border-solid border-[1px] border-[#075B61] opacity-[30%] w-full h-[49px] px-4 text-[#075B61]" />
+                    <input
+                      name="confirm_password"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      type='text'
+                      className="bg-inherit rounded-lg border-solid border-[1px] border-[#075B61] opacity-[30%] w-full h-[49px] px-4 text-[#075B61]" />
                   </div>
                 </div>
-                <div className="bg-inherit grid justify-items-end">
-                  <button className="bg-[#075B61] w-[156px] h-[54px] rounded-[10px] text-white">Daftar</button>
-                </div>
-                <p className="bg-inherit mt-8 text-center text-[#075B61]">Sudah punya akun? Klik <a href="#" className="bg-inherit font-bold">Disini!</a></p>
-              </div>
               
+              <div className="bg-inherit grid justify-items-end">
+                <button
+                  type="submit"
+                  className="bg-[#075B61] w-[156px] h-[54px] rounded-[10px] text-white"
+                  
+                >
+                  Daftar
+                </button>
+              </div>
+              </form>
+              <p className="bg-inherit mt-8 text-center text-[#075B61]">Sudah punya akun? Klik <a href="#" className="bg-inherit font-bold">Disini!</a></p>
             </div>
+
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+
 };
 export default Login;
